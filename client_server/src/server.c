80b6ad38a6da7ac71b2742dffae72bc6880988de
DoricRedPanda <azatoth@ro.ru>
2020-11-17 18:14:40 +0600

    refactoring
    Declare variables at the top of the block.
    Use AF_INET instead of PF_INET.
    Use exit() instead of _exit().
    Use sizeof instead of constant.

--- client_server/src/server.c
+++ client_server/src/server.c
@@ -18,41 +18,47 @@ enum errors {
 };
 
 int init_socket(int port) {
+    int server_socket, socket_option = 1;
+    struct sockaddr_in server_address;
+
     //open socket, return socket descriptor
-    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
         perror("Fail: open socket");
-        _exit(ERR_SOCKET);
+        exit(ERR_SOCKET);
     }
 
     //set socket option
-    int socket_option = 1;
-    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &socket_option, (socklen_t) sizeof(socket_option));
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &socket_option, (socklen_t) sizeof socket_option);
     if (server_socket < 0) {
         perror("Fail: set socket options");
-        _exit(ERR_SETSOCKETOPT);
+        exit(ERR_SETSOCKETOPT);
     }
 
     //set socket address
-    struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(port);
     server_address.sin_addr.s_addr = INADDR_ANY;
-    if (bind(server_socket, (struct sockaddr *) &server_address, (socklen_t) sizeof(server_address)) < 0) {
+    if (bind(server_socket, (struct sockaddr *) &server_address, (socklen_t) sizeof server_address) < 0) {
         perror("Fail: bind socket address");
-        _exit(ERR_BIND);
+        exit(ERR_BIND);
     }
 
     //listen mode start
     if (listen(server_socket, 5) < 0) {
         perror("Fail: bind socket address");
-        _exit(ERR_LISTEN);
+        exit(ERR_LISTEN);
     }
     return server_socket;
 }
 
 
 int main(int argc, char** argv) {
+    int port, server_socket, client_socket;
+    struct sockaddr_in client_address;
+    socklen_t size = sizeof client_address;
+    const char data[] = "VMK";
+
     if (argc != 2) {
         puts("Incorrect args.");
         puts("./server <port>");
@@ -60,27 +66,22 @@ int main(int argc, char** argv) {
         puts("./server 5000");
         return ERR_INCORRECT_ARGS;
     }
-    int port = atoi(argv[1]);
-    int server_socket = init_socket(port);
-    while(1) {
+    port = atoi(argv[1]);
+    server_socket = init_socket(port);
+    for (;;) {
         puts("Wait for connection");
-        struct sockaddr_in client_address;
-        socklen_t size = sizeof client_address;
-        int client_socket = accept(server_socket,
-                                   (struct sockaddr *) &client_address,
-                                   &size);
+        client_socket = accept(server_socket,
+                              (struct sockaddr *) &client_address,
+                              &size);
         printf("connected: %s %d\n", inet_ntoa(client_address.sin_addr),
                                      ntohs(client_address.sin_port));
-        char data[4] = "VMK";
         puts("Send data:");
-        write(client_socket, data, 4);
-        for (int i = 0; i < 4; i++) {
+        write(client_socket, data, sizeof data);
+        for (int i = 0; i < sizeof data; i++) {
             printf("%d ", data[i]);
         }
         puts(data);
         close(client_socket);
     }
-
-
     return OK;
 }
